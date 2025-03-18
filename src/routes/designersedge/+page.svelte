17ef5ddcd01cc620328d7484541d619861283f48
warfarm <danz32321@gmail.com>
2025-03-18 18:12:35 -0400

    Add FAQ section

--- src/routes/designersedge/+page.svelte
+++ src/routes/designersedge/+page.svelte
@@ -2,7 +2,7 @@
     import Separator from "$lib/components/Separator.svelte";
     const formUrl = "https://forms.gle/HxASYdmcsKcadv9CA";
 
-    //info section details
+    // info section details
     let details = [
         { title: "30 HOURS", subtitle: "of designing" },
         { title: "APRIL 2025", subtitle: "5th ~ 6th" },
@@ -10,7 +10,7 @@
         { title: "THEME", subtitle: "To be Revealed 🤫" }
     ];
 
-    //info on competition
+    // info on competition
     let competitionDetails = [
         { 
             title: "Workshops", 
@@ -33,6 +33,33 @@
             logo: "📝"
         }
     ];
+    
+    // faq info
+    let faqInfo = [
+
+        {
+            question: "What software am I allowed to use?",
+            answer: "You are allowed to utilize any software or applications you have access to or that we provide to create your project. However, the usage of any AI generative software or any software alike will restrict your project to only be judged for the AI category."
+        },
+        {
+            question: "What do I need to bring?",
+            answer: "All you need to bring is your laptop! The software that we use will be free or provided to you for the competition. Note that some of the software may not run well on older computers so we encourage you to check your computer specifications with the recommended specifications of some software we may be using such as blender."
+        },
+        {
+            question: "Who can participate?",
+            answer: "Any Binghamton student of any major and year can participate as long as they’re interested! No prior experience is necessary and we will have a novice and experienced bracket for people to compete in."
+        },
+        {
+            question: "Will there be food?",
+            answer: "Yes, there will be food and drinks provided at the venue! We will have catering from establishments in the area."
+        },
+        {
+            question: "Are there registration fees?",
+            answer: "This competition is free to attend! Just register using the form above and check in at the venue."
+        },
+
+    ];
+
 </script>
 
 <!-- Header Section -->
@@ -90,7 +117,7 @@
 <Separator />
 
 <div class="flex flex-col w-screen h-full font-plex-mono text-white font-bold md:px-20 px-5 pb-32">
-    <h2 class="flex md:text-5xl text-2xl w-full items-end custom-height-md md:pt-0 pt-10">Join the Challenge</h2>
+    <h2 class="flex md:text-5xl text-3xl w-full items-end custom-height-md md:pt-0 pt-10">Join the Challenge</h2>
 
     <!-- Embedded Form -->
     <div class="mt-10 md:mt-16 w-full flex justify-center">
@@ -109,4 +136,19 @@
     </div>
 </div>
 
-  
+<Separator />
+
+<div class = "flex flex-col w-full h-full items-center font-bold font-plex-mono text-white">
+    <h1 class = "font-extrabold flex md:text-5xl text-3xl underline">
+        FAQ
+    </h1>
+    <!-- FAQ Section -->
+    <div class="flex md:flex-col flex-row mt-4 items-center grid place-items-center">
+        {#each faqInfo as info}
+            <div class="flex flex-col items-center text-center justify-center md:gap-6 gap-4 md:pt-10 pt-5 md:px-20 px-10">
+                <p class="text-green text-2xl font-bold underline">{info.question}</p>
+                <p class="text-orange text-xl">{info.answer}</p>
+            </div>
+        {/each}
+    </div>
+</div>
